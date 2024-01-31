f862bf61201fc664f281fd3a6b1790d8718aa26f
Marcos Buganeme <marcos.buganeme@ssquadra.com.br>
2024-01-30 23:05:51 -0300

    feat(api) : modelando a entidade order

--- src/orders/entities/order.entity.ts
+++ src/orders/entities/order.entity.ts
@@ -1,1 +1,56 @@
-export class Order {}
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  OneToMany,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+import { OrderItem } from './order-item.entity';
+
+export enum OrderStatus {
+  PENDING = 'pending',
+  PAID = 'paid',
+  FAILED = 'failed',
+}
+
+export type CreateOrderCommand = {
+  client_id: number;
+  items: {
+    product_id: string;
+    quantity: number;
+    price: number;
+  }[];
+};
+
+@Entity('tb_order')
+export class Order {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ type: 'decimal', precision: 10, scale: 2 })
+  total: number;
+
+  @Column()
+  client_id: number;
+
+  @Column()
+  status: OrderStatus = OrderStatus.PENDING;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @OneToMany(() => OrderItem, (item) => item.order)
+  items: OrderItem[];
+
+  static create(input: CreateOrderCommand) {
+    const order = new Order();
+    order.client_id = input.client_id;
+    order.items = input.items.map((item) => {
+      const orderItem = new OrderItem();
+      orderItem.product_id = item.product_id;
+      orderItem.quantity = item.quantity;
+      orderItem.price = item.price;
+      return orderItem;
+    });
+  }
+}
