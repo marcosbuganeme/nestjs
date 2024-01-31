cb8fdfe6e51a4b0bb8bb41c905104b5b8d1687d6
Marcos Buganeme <marcos.buganeme@ssquadra.com.br>
2024-01-31 11:31:00 -0300

    feat (api): adicionando regra de negócio inicial de criação do pedido

--- src/orders/orders.service.ts
+++ src/orders/orders.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import { Product } from 'src/products/entities/product.entity';
+import { In, Repository } from 'typeorm';
 import { CreateOrderDto } from './dto/create-order.dto';
 import { Order } from './entities/order.entity';
 
@@ -8,11 +9,32 @@ import { Order } from './entities/order.entity';
 export class OrdersService {
   constructor(
     @InjectRepository(Order) private orderRepository: Repository<Order>,
+    @InjectRepository(Order) private productRepository: Repository<Product>,
   ) {}
 
-  create(createOrderDto: CreateOrderDto) {
-    const order = this.orderRepository.create(createOrderDto);
-    return this.orderRepository.save(order);
+  async create(createOrderDto: CreateOrderDto) {
+    const productIds = createOrderDto.items.map((item) => item.product_id);
+    const uniqueProductIds = [...new Set(productIds)];
+    const products = await this.productRepository.findBy({
+      id: In(uniqueProductIds),
+    });
+
+    Order.create({
+      client_id: 1,
+      items: createOrderDto.items.map((item) => {
+        const product = products.find(
+          (product) => product.id === item.product_id,
+        );
+
+        return {
+          price: product.price,
+          product_id: item.product_id,
+          quantity: item.quantity,
+        };
+      }),
+    });
+
+    return '';
   }
 
   findAll() {
