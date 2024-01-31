d1f2c5671b4b5848f949a9e312e985c0f4fde8d3
Marcos Buganeme <marcos.buganeme@ssquadra.com.br>
2024-01-30 22:19:06 -0300

    feat (api): adicionadno typeormmodule product

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
-import { ProductsService } from './products.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Product } from './entities/product.entity';
 import { ProductsController } from './products.controller';
+import { ProductsService } from './products.service';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Product])],
   controllers: [ProductsController],
-  providers: [ProductsService]
+  providers: [ProductsService],
 })
 export class ProductsModule {}
