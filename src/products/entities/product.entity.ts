83e46eec8d85b9b568a46bd204c1bbc258fb0207
Marcos Buganeme <marcos.buganeme@ssquadra.com.br>
2024-01-30 21:46:23 -0300

    feat (api): modelando entidade de produto

--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -1,1 +1,19 @@
-export class Product {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('tb_product')
+export class Product {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  description: string;
+
+  @Column()
+  image_url: string;
+
+  @Column()
+  price: number;
+}
