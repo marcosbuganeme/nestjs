a6af74e6987f5584d59daa14252161537fff486e
Marcos Buganeme <marcos.buganeme@ssquadra.com.br>
2024-02-01 20:10:39 -0300

    feat (api) : habilitando autenticação nas rotas de orders

--- web-api/src/orders/orders.controller.ts
+++ web-api/src/orders/orders.controller.ts
@@ -1,23 +1,37 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Post,
+  Req,
+  UseGuards,
+} from '@nestjs/common';
+import { Request } from 'express';
+import { AuthGuard } from 'src/auth/auth.guard';
 import { CreateOrderDto } from './dto/create-order.dto';
 import { OrdersService } from './orders.service';
 
+@UseGuards(AuthGuard)
 @Controller('orders')
 export class OrdersController {
   constructor(private readonly ordersService: OrdersService) {}
 
   @Post()
-  create(@Body() createOrderDto: CreateOrderDto) {
-    return this.ordersService.create(createOrderDto);
+  create(@Body() createOrderDto: CreateOrderDto, @Req() req: Request) {
+    return this.ordersService.create({
+      ...createOrderDto,
+      client_id: req['user'].sub,
+    });
   }
 
   @Get()
-  findAll() {
-    return this.ordersService.findAll();
+  findAll(@Req() req: Request) {
+    return this.ordersService.findAll(req['user'].sub);
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.ordersService.findOne(id);
+  findOne(@Param('id') id: string, @Req() req: Request) {
+    return this.ordersService.findOne(id, req['user'].sub);
   }
 }
