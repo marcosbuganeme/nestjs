96c87c8856a8a366782d5eb07425e688764a8e10
Marcos Buganeme <marcos.buganeme@ssquadra.com.br>
2024-02-05 19:30:40 -0300

    fix: modificando servidor para refletir os serviços docker-compose

--- web-api/src/app.module.ts
+++ web-api/src/app.module.ts
@@ -1,20 +1,18 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
 import { OrderItem } from './orders/entities/order-item.entity';
 import { Order } from './orders/entities/order.entity';
 import { OrdersModule } from './orders/orders.module';
 import { Product } from './products/entities/product.entity';
 import { ProductsModule } from './products/products.module';
-import { AuthModule } from './auth/auth.module';
 import { RabbitmqModule } from './rabbitmq/rabbitmq.module';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: 'localhost',
+      host: 'mysql',
       port: 3306,
       username: 'root',
       password: 'root',
@@ -28,7 +26,5 @@ import { RabbitmqModule } from './rabbitmq/rabbitmq.module';
     AuthModule,
     RabbitmqModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
--- web-api/src/rabbitmq/rabbitmq.module.ts
+++ web-api/src/rabbitmq/rabbitmq.module.ts
@@ -1,4 +1,13 @@
-import { Module } from '@nestjs/common';
+import { RabbitMQModule } from '@golevelup/nestjs-rabbitmq';
+import { Global, Module } from '@nestjs/common';
 
-@Module({})
+@Global()
+@Module({
+  imports: [
+    RabbitMQModule.forRoot(RabbitMQModule, {
+      uri: 'amqp://admin:admin@rabbitmq:5672',
+    }),
+  ],
+  exports: [RabbitMQModule],
+})
 export class RabbitmqModule {}
